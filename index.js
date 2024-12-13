79a8a1afa8887521b1eabeb7edd74e2709e816f2
santino2221 <166014288+santino2221@users.noreply.github.com>
2024-12-13 08:20:58 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+let nombre = ("juan")
+let edad = (25)
+let ciudad = ("madrid")
+let numero1 =
+letnumero2=
+letsuma =(numero1+numero2)
+let hola =("santino")
+let ,nombre,usuario =
+let ,mensaje,completo =
+console.log = "hola,nombre,mensaje completo"
+
+
+console.log =(numero1+numero2)
+
+
+
+
+console.log(nombre,edad,ciudad)
